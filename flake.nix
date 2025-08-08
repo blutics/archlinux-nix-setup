cb53b304c80413fe9f48af9d93b0192cdcd0cff1
Daegun Kim <blutics@gmail.com>
2025-08-08 17:58:09 +0900

    update

--- flake.nix
+++ flake.nix
@@ -1,10 +1,13 @@
 {
-  description = "Home Manager flake";
+  description = "WSL + Arch + Nix + Home Manager + Neovim Setup";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, home-manager, ... }: {
